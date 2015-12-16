1f4ee23a3b5947c231633df9978f9e0b3221f290
Alximick <Alexey@MacBook-Air.local>
2015-12-16 16:09:25 +0600

    unicode

---
+++ sibsutis-python-class/unicode1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python -tt
+# encoding: utf-8
+
+# Реализуйте функцию, которая возвращала бы побайтовое 
+# представление строки в заданной кодировке
+# в виде списка чисел в десятичной системе счисления
+# 
+# Подсказка: ord(x) возвращает десятичное значение кода символа x
+import sys
+def ordinal(text, encoding='utf-8'):
+    # +++your code here+++
+    but = ''
+    test = '['
+    bu = text.encode(encoding)
+    for i in bu:
+        but = str(ord(i))
+        test += but +','+' '
+    return test[0:-2]+']'
+
+def test(got, expected):
+    print('{} got: {} expected: {}'.format(
+       ' OK ' if got == expected else '  X ', 
+       repr(got), repr(expected)))
+
+
+if __name__ == '__main__':
+    test(ordinal('cat'), [99, 97, 116])
+    test(ordinal(u'кот', 'cp1251'), [234, 238, 242])
+    test(ordinal(u'кот', 'utf8'), [208, 186, 208, 190, 209, 130])
+    test(ordinal(u'γάτα', 'cp1253'), [227, 220, 244, 225])
+    test(ordinal(u'côté', 'latin1'), [99, 244, 116, 233])
+    test(ordinal('cat', 'utf16'), [255, 254, 99, 0, 97, 0, 116, 0])
+
